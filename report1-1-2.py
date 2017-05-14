bedb11e3512c4cc230726da878153c831498139a
tachemac <tachemac@gmail.com>
2017-05-14 13:00:23 +0900

    refactoring

--- report1-1-2.py
+++ report1-1-2.py
@@ -22,23 +22,13 @@ for i in range(max_theta1):
     E3 = eps2 * math.cos(theta1) - eps1 * math.sqrt(n*n - math.sin(theta1))
     E2 = 2 * n * eps1 * math.cos(theta1)
     E1 = eps2 * math.cos(theta1) + eps1 * math.sqrt(n*n - math.sin(theta1))
-    r1 = E3/E1
-    t1 = E2/E1
-    if r1 >= 0:
-        R1.append(r1)
-    else:
-        R1.append(-r1)
-        
-    if t1 >= 0:
-        T1.append(t1)
-    else:
-        T1.append(-t1)
-
+    R1.append(math.fabs(E3/E1))
+    T1.append(math.fabs(E2/E1))
 
 x = range(max_theta1)
 plt.plot(x, R1, label="Reflection coef.")
 plt.plot(x, T1, label="Transmission coef.")
 plt.legend(loc="center left")
 plt.xlabel(r"$\theta_1 [degree]$")
-plt.savefig('abosan1-1-2.png')
+# plt.savefig('abosan1-1-2.png')
 plt.show()
