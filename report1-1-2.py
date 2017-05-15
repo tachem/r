c9226d8c6f3e23c51c747a75feeee9377e6ba456
take <take>
2017-05-15 19:56:39 +0900

    fix bug

--- report1-1-2.py
+++ report1-1-2.py
@@ -1,5 +1,5 @@
 '''
-abo-san class.
+Abo-san class.
 Report1 2017/4/18
 Answer of Probrem 1-2.
 '''
@@ -19,9 +19,9 @@ R1 = []
 T1 = []
 for i in range(max_theta1):
     theta1 = toRad(float(i))
-    E3 = eps2 * math.cos(theta1) - eps1 * math.sqrt(n*n - math.sin(theta1))
+    E3 = eps2 * math.cos(theta1) - eps1 * math.sqrt(n*n - math.sin(theta1)**2)
     E2 = 2 * n * eps1 * math.cos(theta1)
-    E1 = eps2 * math.cos(theta1) + eps1 * math.sqrt(n*n - math.sin(theta1))
+    E1 = eps2 * math.cos(theta1) + eps1 * math.sqrt(n*n - math.sin(theta1)**2)
     R1.append(math.fabs(E3/E1))
     T1.append(math.fabs(E2/E1))
 
@@ -31,5 +31,5 @@ plt.plot(x, T1, label="Transmission coef.")
 plt.legend(loc="center left")
 plt.xlabel(r"$\theta_1 [degree]$")
 plt.title('air to water')
-# plt.savefig('abosan1-1-2.png')
+plt.savefig('abosan1-1-2.png')
 plt.show()
