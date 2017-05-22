7afc301a2b2345047ec6bdd0afb56b026d6a6a51
take <take>
2017-05-22 22:20:31 +0900

    add repo5.png

--- report1-1-2.py
+++ report1-1-2.py
@@ -10,15 +10,13 @@ import math
 eps1 = 1.
 eps2 = 80.
 n = math.sqrt(eps2/eps1)
-
-def toRad(x_degree):
-    return x_degree  * 2. * math.pi / 360.
+to_radian = math.pi/180.
 
 max_theta1 = 90
 R1 = []
 T1 = []
 for i in range(max_theta1):
-    theta1 = toRad(float(i))
+    theta1 = float(i) * to_radian
     E3 = eps2 * math.cos(theta1) - eps1 * math.sqrt(n*n - math.sin(theta1)**2)
     E2 = 2 * n * eps1 * math.cos(theta1)
     E1 = eps2 * math.cos(theta1) + eps1 * math.sqrt(n*n - math.sin(theta1)**2)
@@ -31,5 +29,5 @@ plt.plot(x, T1, label="Transmission coef.")
 plt.legend(loc="center left")
 plt.xlabel(r"$\theta_1 [degree]$")
 plt.title('air to water')
-plt.savefig('abosan1-1-2.png')
+# plt.savefig('abosan1-1-2.png')
 plt.show()
